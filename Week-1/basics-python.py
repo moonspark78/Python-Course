fce7bfd0c076663863ff379172f24073921ada9c
Omar <jabour.omar@hotmail.com>
2025-09-27 16:31:42 +0200

    :art: working in  the basics-python file v.01 :art:

--- Week-1/basics-python.py
+++ Week-1/basics-python.py
@@ -1,3 +1,7 @@
 # comment
 print('The above statement is a comment')
 print ("first", "second", 2021, True, [1, 2, 3])
+# python has many built-in functions exemple print(), len(), type(), input() etcs
+print(len("Hello World"))
+print(type(3.14))
+print(input("Enter your name: "))
