3ff69f7334f3880855135494d559fc042cc67d3e
Omar <jabour.omar@hotmail.com>
2025-09-28 12:47:20 +0200

    :art: working on  the bultin-functions file v.07 :art:

--- Week-1/bultin-functions.py
+++ Week-1/bultin-functions.py
@@ -1,14 +1,15 @@
 #print(), len(), input(), type(), round()
 
-print("I love using print because it shows me what is happening")
+
+""" print("I love using print because it shows me what is happening")
 print("Asabeneh")
 print(len("Asabeneh"))
 print(type("Asabeneh"))
 print(len("eye bail"))
-print(len(["heejd","frne","totmr"]))
+print(len(["heejd","frne","totmr"])) """
 
 #variable are a place where to store data
-first_name = input("What is your first name? ")
+""" first_name = input("What is your first name? ")
 age = input("How old are you? ")
 print(first_name)
-print(age)
+print(age) """
