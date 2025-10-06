055aad3257f4376c21ff3573aa90405482beca00
Omar <jabour.omar@hotmail.com>
2025-10-06 21:10:02 +0200

    :art: working on the Week-2 arithmetic file v.03  :art:

--- Week-2/arithmetic-operators.py
+++ Week-2/arithmetic-operators.py
@@ -5,6 +5,16 @@ b = 3
 
 total = a + b          # Addition
 difference = a - b   # Subtraction
+product = a * b       # Multiplication
+quotient = a / b      # Division
+remainder = a % b     # Modulus
+floor_div = a // b    # Floor Division
+exponent = a ** b     # Exponentiation
 
 print("The total is: ", total)
 print("The difference is: ", difference)
+print("The product is: ", product)
+print("The quotient is: ", quotient)
+print("The remainder is: ", remainder)
+print("The floor division is: ", floor_div)
+print("The exponent is: ", exponent)
