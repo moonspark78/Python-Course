a1ea5325ba2e9115e59ec47bb4597ba50340abb9
Omar <jabour.omar@hotmail.com>
2025-10-11 21:30:25 +0200

    :art: working on  the file logical-operators v.06  :art:

--- Week-2/logical-operators.py
+++ Week-2/logical-operators.py
@@ -12,5 +12,10 @@ print(2<1 and 4>3) # False and True => False
 #or
 print(10>5)
 print(5<8)
-print(10<5 or 5<8) # False and True => False
+
+print(10<5 or 5<8) # False or True => True
+print(10>5 or 5>8) # True or True => True
+print(10<5 or 5>8) # False or False => False
+
+
 
