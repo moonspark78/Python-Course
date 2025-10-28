785492cdac0e178df37c7f5550da648e29826243
Omar <jabour.omar@hotmail.com>
2025-10-28 20:08:48 +0100

    :art: Week-3 : List  working v.3  :art:

--- Week-3/lists.py
+++ Week-3/lists.py
@@ -1,7 +1,7 @@
 # List
 # ['item1', 'item2', 'item3']
 
-shopping_list = ['milk']
+shopping_list = ['milk', 'eggs', 'bread', 'butter']
 print(shopping_list)
 print(type(shopping_list))
 print(len(shopping_list))
