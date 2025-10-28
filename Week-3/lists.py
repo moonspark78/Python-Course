3fac803fe3432e9c109a70e9b7e14ee1e7ccf7c8
Omar <jabour.omar@hotmail.com>
2025-10-28 20:27:22 +0100

    :art: Week-3 : List  working v.4  :art:

--- Week-3/lists.py
+++ Week-3/lists.py
@@ -5,3 +5,9 @@ shopping_list = ['milk', 'eggs', 'bread', 'butter']
 print(shopping_list)
 print(type(shopping_list))
 print(len(shopping_list))
+
+nums = [1, 2, 3, 4, 5]
+print(nums)
+
+names = ['Alice', 'Bob', 'Charlie']
+print(names)
